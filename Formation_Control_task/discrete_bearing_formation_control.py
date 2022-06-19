9bda1a88580904478e14280fb71ba00e8fda4543
Alessandro Cecconi <ale.c.cecconi@gmail.com>
2022-06-19 23:51:01 +0200

    1st commit from Alex' Desktop

---
+++ Formation_Control_task/discrete_bearing_formation_control.py
@@ -0,0 +1,59 @@
+import numpy as np
+import networkx as nx
+import control as ctrl
+import matplotlib.pyplot as plt
+from functions import *
+
+# graph is time-varying, but let's build the simplest case to try the algorithm and then move formard
+NN = 4
+d = 2
+n_leaders = 2
+
+# Initialize Bearing Laplacian
+B = np.zeros((d*NN,d*NN))
+# it stores all the vector position for each node
+# to extract a specific vector position you should do Node_pos[i-th node,:,:]
+Node_pos = np.zeros((NN,d,1)) # it is a tensor with NN matrix of dimension (dxd)
+
+# try to reproduce a rectangle
+# Node 0, ( px py ).T
+Node_pos[0,:,:] = np.array([[
+    1,5]
+    ]).T
+# Node 1 (px py).T
+Node_pos[1,:,:] = np.array([
+    [5,5]
+    ]).T
+# Node 2 (px py).T
+Node_pos[2,:,:] = np.array([[
+    1,1]
+    ]).T
+# Node 3 (px py).T
+Node_pos[3,:,:] = np.array([
+    [5,1]
+    ]).T
+
+# it stores all the matrices Pg_ij ( d x d )
+# to extract Pg_ij you must Pg_stack[node_i, node_j,:,:]
+Pg_stack = np.zeros((NN,NN,d,d)) # it is a tensor of dimension 4 NN,NN of dimension (dxd)
+
+# for cycles to pupulate the tensor Pg_stack
+for node_i in range(NN):
+    for node_j in range(NN):
+        pos_j = Node_pos[node_j,:,:]
+        pos_i = Node_pos[node_i,:,:]
+        Pg_ij = proj_matrix(pos_i, pos_j)
+        Pg_stack[node_i,node_j,:,:] = Pg_ij
+
+## Pg_ij is always the same, but the Matrix B is computed according to the Adjacency Matrix of the graph
+G = nx.binomial_graph(NN, 0.7)
+Adj = nx.adjacency_matrix(G).toarray()
+print(Adj)
+fig = plt.figure()
+nx.draw(G,with_labels= True)
+plt.show()
+
+B = bearing_dynamics(np.random.randn((d*NN)), Pg_stack, Adj, d)
+print(B)
+print(B.shape)
+
--- Formation_Control_task/functions.py
+++ Formation_Control_task/functions.py
@@ -1,6 +1,86 @@
 import numpy as np
 import networkx as nx
 
-def formation_dynamics(x, Adj, dist):
-    x_dot = np.zeros(np.size(x))
-    return pass
+def bearing_vector(vec_pi:np.array, vec_pj:np.array, d = None):
+    # vec_p is a position vector of dimension dx1
+    # We have to check it
+    row_i = np.shape(vec_pi)[0]
+    row_j= np.shape(vec_pj)[0]
+    if d: # if we impose the dimension we'll enter this if, otherwise we'll move to the next statement
+        if row_i == row_j and row_i == d:
+            pass
+        else:
+            print("Vectors dimensions are con consistent")
+    elif row_i == row_j and row_i == 2: # we want to check if the two vectors have consistent dimension (fixed one)
+        pass
+    else:
+        print("WARNING! The dimension of the vector are not consistent. Check them")
+    g_ij = (vec_pj - vec_pi) / np.linalg.norm(vec_pj - vec_pi) # unit distance vector
+    return g_ij
+
+
+def proj_matrix(vec_pi:np.array, vec_pj:np.array, d = None):
+    # Function that computes the Projection Matrix based on the vectors p_i and p_j
+    if d:
+        g_ij = bearing_vector(vec_pi, vec_pj, d)
+        Id = np.identity(d)
+        Pg_ij = Id - g_ij@g_ij.T
+    else:
+        g_ij = bearing_vector(vec_pi, vec_pj)
+        d1 = np.shape(g_ij)[0]
+        Id = np.identity(d1)
+        Pg_ij = Id - g_ij@g_ij.T
+    return Pg_ij
+
+"""def bearing_dynamics(x, Pg_stack, Adj, d):
+    N, _ = np.shape(Adj)
+    nx = np.size(x)
+    B = np.zeros((d*N,d*N))
+
+    if N == nx/2: # check if the vector dimension if consistent
+        for node_i in range(N):
+            list_neigh_i = np.nonzero(Adj[node_i])[0] # neighbors of the node i
+            list_index_i = node_i*d + np.arange(d)
+            for node_j in list_neigh_i:
+                list_index_j = node_j*d + np.arange(d)
+                if node_i != node_j:
+                    Pg_ij = Pg_stack[node_i,node_j,:,:]
+                    B[list_index_i, list_index_j] = - Pg_ij
+
+
+
+    return None"""
+
+
+def bearing_dynamics(x:np.array, Pg_stack, Adj:np.array, d:int):
+    n_agents = np.shape(Adj)[0] # int representing number of agents
+    nx = np.size(x) # dimension of vector x
+    B_temp = np.zeros((n_agents,n_agents,d,d)) # tensor of 4 dimension to build matrix B
+    if n_agents == nx/d: # dimension check
+        for node_i in range(n_agents):
+            list_neigh_i = np.nonzero(Adj[node_i])[0] # neighbors of node i
+            for node_j in range(n_agents):
+                if node_j in list_neigh_i: # if the node j is a neighbor
+                    if node_j != node_i: # if the nodes are not the sa,e
+                        B_temp[node_i, node_j,:,:] = - Pg_stack[node_i, node_j,:,:]
+                    elif node_i == node_j: # if the node are the same (block diagonal of matrix B)
+                        B_temp[node_i,node_j] = np.sum([Pg_stack[node_i, k, :, :] for k in list_neigh_i])
+
+        B = np.zeros((d*n_agents,d*n_agents))
+        # another cycle to rebuild the matrix B
+        for i in range(n_agents):
+            for j in range(n_agents):
+                for k in range(d):
+                    for z in range(d):
+                        B[i*d+k,j*d+z] = B_temp[i,j,k,z] # rebuild the matrix with the right shape
+        return B
+
+    else:
+        print("Error in Bearing Dynamics: dimension of the vector x is not consistent with the number of agents")
+
+
+
+
+
+
+
