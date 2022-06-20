7daaf73254cdd0e3cf87168a701f048f8032d68e
Alessandro Cecconi <ale.c.cecconi@gmail.com>
2022-06-20 10:40:42 +0200

    commit from Alex' Desktop

--- Formation_Control_task/discrete_bearing_formation_control.py
+++ Formation_Control_task/discrete_bearing_formation_control.py
@@ -5,15 +5,20 @@ import matplotlib.pyplot as plt
 from functions import *
 
 # graph is time-varying, but let's build the simplest case to try the algorithm and then move formard
-NN = 4
-d = 2
+NN = 4 # number of agents
+d = 2 # dimension of the pos/vel vector (2-dimensional or 3-dimension motion)
 n_leaders = 2
+dt = int(10e-4)
 
-# Initialize Bearing Laplacian
-B = np.zeros((d*NN,d*NN))
-# it stores all the vector position for each node
+
+P0 = np.random.rand((d*NN)) # random initial conditions for position and velocity of all the agents
+V0 = np.random.rand((d*NN))
+## NB X = [ p, v].T where p is a vector d*N and v is a vector d*N, x is a vector 2*d*N
+
+B = np.zeros((d*NN,d*NN)) # Initialization of Bearing Laplacian
+Node_pos = np.zeros((NN,d,1)) #it stores all the vector position for each node, it is a tensor with NN matrix of dimension (dxd),
 # to extract a specific vector position you should do Node_pos[i-th node,:,:]
-Node_pos = np.zeros((NN,d,1)) # it is a tensor with NN matrix of dimension (dxd)
+
 
 # try to reproduce a rectangle
 # Node 0, ( px py ).T
@@ -33,11 +38,10 @@ Node_pos[3,:,:] = np.array([
     [5,1]
     ]).T
 
-# it stores all the matrices Pg_ij ( d x d )
+Pg_stack = np.zeros((NN,NN,d,d)) # it stores all the matrices Pg_ij ( d x d ), it is a tensor of dimension 4 NN,NN of dimension (dxd)
 # to extract Pg_ij you must Pg_stack[node_i, node_j,:,:]
-Pg_stack = np.zeros((NN,NN,d,d)) # it is a tensor of dimension 4 NN,NN of dimension (dxd)
 
-# for cycles to pupulate the tensor Pg_stack
+# for cycles to populate the tensor Pg_stack
 for node_i in range(NN):
     for node_j in range(NN):
         pos_j = Node_pos[node_j,:,:]
@@ -53,8 +57,10 @@ fig = plt.figure()
 nx.draw(G,with_labels= True)
 plt.show()
 
-B = bearing_dynamics(np.random.randn((d*NN)), Pg_stack, Adj, d)
+B = bearing_laplacian(Pg_stack, Adj, d)
+p_plus, v_plus = bearing_dynamics(P0,V0,B,dt)
+
 print(B)
 print(B.shape)
-
+print("p_t+1 ={}".format(p_plus), "v_t+1={}".format(v_plus))
 
--- Formation_Control_task/functions.py
+++ Formation_Control_task/functions.py
@@ -52,37 +52,40 @@ def proj_matrix(vec_pi:np.array, vec_pj:np.array, d = None):
     return None"""
 
 
-def bearing_dynamics(x:np.array, Pg_stack:np.array, Adj:np.array, d:int):
-    # function which computes the bearing dynamics of the reference paper
-    # first it computes the matrix B(G(p))
-    # then discretize the dynamics according to forward Euler
+def bearing_laplacian(Pg_stack:np.array, Adj:np.array, d:int):
+    # function which computes the bearing laplacian
+    # it computes the matrix B(G(p))
     n_agents = np.shape(Adj)[0] # int representing number of agents
-    nx = np.size(x) # dimension of vector x
     B_temp = np.zeros((n_agents,n_agents,d,d)) # tensor of 4 dimension to build matrix B
-    if n_agents == nx/d: # dimension check
-        for node_i in range(n_agents):
-            list_neigh_i = np.nonzero(Adj[node_i])[0] # neighbors of node i
-            print("neighbors of node i are.{}".format(list_neigh_i))
-            for node_j in range(n_agents):
-                if node_j in list_neigh_i: # if the node j is a neighbor
-                    if node_j != node_i: # if the nodes are not the sa,e
-                        B_temp[node_i, node_j,:,:] = - Pg_stack[node_i, node_j,:,:]
-                elif node_i == node_j: # if the node are the same (block diagonal of matrix B)
-                    for node_k in list_neigh_i:
-                        B_temp[node_i, node_j,:,:] += Pg_stack[node_i, node_k,:,:] # summation of the matrices Pg_ik
+    for node_i in range(n_agents):
+        list_neigh_i = np.nonzero(Adj[node_i])[0] # neighbors of node i
+        for node_j in range(n_agents):
+            if node_j in list_neigh_i: # if the node j is a neighbor
+                if node_j != node_i: # if the nodes are not the sa,e
+                    B_temp[node_i, node_j,:,:] = - Pg_stack[node_i, node_j,:,:]
+            elif node_i == node_j: # if the node are the same (block diagonal of matrix B)
+                for node_k in list_neigh_i:
+                    B_temp[node_i, node_j,:,:] += Pg_stack[node_i, node_k,:,:] # summation of the matrices Pg_ik
 
+    B = np.zeros((d*n_agents,d*n_agents))
+    # another cycle to build the matrix B with the right dimensions
+    for i in range(n_agents):
+        for j in range(n_agents):
+            for k in range(d):
+                for z in range(d):
+                    B[i*d+k,j*d+z] = B_temp[i,j,k,z]
+    return B
 
-        B = np.zeros((d*n_agents,d*n_agents))
-        # another cycle to build the matrix B with the right dimensions
-        for i in range(n_agents):
-            for j in range(n_agents):
-                for k in range(d):
-                    for z in range(d):
-                        B[i*d+k,j*d+z] = B_temp[i,j,k,z]
-        return B
-
+def bearing_dynamics(p_t:np.array, v_t:np.array, B, dt):
+    # function which computes the forward-euler discretization of the Bearing Laplacian Model
+    n_p = np.shape(p_t)[0]
+    n_v = np.shape(v_t)[0]
+    if n_p == n_v: # check the dimension of position and velocity vector
+        pos_plus = p_t + dt*(B@p_t)
+        vel_plus = v_t + dt*(B@v_t)
+        return pos_plus, vel_plus
     else:
-        print("Error in Bearing Dynamics: dimension of the vector x is not consistent with the number of agents")
+        print("Error in Bearing Dynamics: Pos vector and Vel vector dimensions are not consistent")
 
 
 
