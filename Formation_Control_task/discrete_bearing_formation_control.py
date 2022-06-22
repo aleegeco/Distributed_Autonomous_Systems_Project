5320c493cae74f396b3270648c3683a64c83b6bb
Alessandro Cecconi <59066644+aleegeco@users.noreply.github.com>
2022-06-22 17:03:04 +0200

    Messo calcolo Kron Matrix in una funzione

--- Formation_Control_task/discrete_bearing_formation_control.py
+++ Formation_Control_task/discrete_bearing_formation_control.py
@@ -12,16 +12,14 @@ n_leaders = 2  # number of leaders
 p_ER = 0.9
 n_follower = NN - n_leaders  # number of followers
 
-dt = 0.1  # discretion step
-T_max = 50  # tempo massimo simulation
+dt = 0.1  # discretization step
+T_max = 50  # max simulation time
 horizon = np.linspace(0.0, T_max, int(T_max / dt))
-max_iter = 500  # number of iterations
+max_iter = 500  # max number of iterations
 
 # gain parameters
 k_p = 0.7
 k_v = 1.5
-
-# B = np.zeros((d * NN, d * NN))  # Initialization of Bearing Laplacian
 
 Node_pos = np.zeros((NN, dd, 1))  # it stores all the vector position for each node
 # it is a tensor with NN matrix of dimension (dxd)
@@ -45,13 +43,13 @@ Node_pos[3, :, :] = np.array([
     [5, 1]
 ]).T
 
-
-# Pg_ij is always the same, but the Matrix B is computed according to the Adjacency Matrix of the graph
+# Graph and Adjacency matrix
 G = nx.binomial_graph(NN, p_ER)
-
 Adj = nx.to_numpy_array(G)
 
+# Stack of all Pg_ij
 Pg_stack = proj_stack(Node_pos, NN, dd)
+# Bearing Laplacian Matrix
 B = bearing_laplacian(Pg_stack, Adj, dd)
 
 if FIGURE:
@@ -59,10 +57,11 @@ if FIGURE:
     nx.draw(G, with_labels=True)
     plt.show()
 
+### TEST ###
+
 Bff = B[dd * n_leaders:dd * NN, dd * n_leaders:dd * NN]
 Bfl = B[dd * n_leaders:dd * NN, :dd * n_leaders]
 
-### TEST ###
 pos_leaders = Node_pos[[0, 1], :, :].reshape([4, 1])
 if np.linalg.det(Bff) != 0:
     print("The matrix Bff is not singular")
@@ -72,22 +71,24 @@ else:
 ###
 
 
-## Initialization of state vector and reference vector
+## Initialization of state vector, reference vector and init vector
 xx = np.zeros((NN * dd * 2, len(horizon)))
 xx_star = np.zeros((NN * dd * 2, len(horizon)))
+xx_init = np.zeros((NN * 2 * dd, 1))
 
+# Kroenecker Function of the dynamic
 A_kron = kron_dynamical_matrix(B, NN, n_leaders, k_p, k_v, dd)
 
+# We impose the leaders position
 for i in range(NN):
     for j in range(dd):
         xx_star[(n_leaders * i) + j] = Node_pos[i, j, :]
 
-xx_init = np.zeros((NN * 2 * dd, 1))
+### ???? Non l'hai già fatto prima?
 for i in range(n_leaders):
     for j in range(dd):
         xx_init[(n_leaders * i) + j] = Node_pos[i, j, :]
 
-### ???? Non l'hai già fatto prima?
 xx[:, 0] = xx_init.reshape(16)
 
 for tt in range(len(horizon) - 1):
