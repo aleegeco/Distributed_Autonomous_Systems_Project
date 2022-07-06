d2561fe44181576b705d336ef5c51bbcaedac76d
Marco Bugo <marco.bugo.98@gmail.com>
2022-07-06 21:57:21 +0200

    Sembra molto buonoooo

---
+++ Test_Marco/ActivationFunctions.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+def sigmoid_fn(xi):
+    return 1 / (1 + np.exp(-xi))
+
+
+def sigmoid_fn_derivative(xi):
+    return sigmoid_fn(xi) * (1 - sigmoid_fn(xi))
+
+
+def ReluPlus(xi):
+    if xi >= 0:
+        return xi
+    else:
+        return 0.1 * xi
+
+
+def ReluPlus_derivative(xi):
+    if xi >= 0:
+        return 1
+    else:
+        return 0.1
+
+
+def sigmoid_fnPosNeg(xi):
+    return 2 / (1 + np.exp(-xi)) - 1
+
+
+# Derivative of Activation Function
+def sigmoid_fn_derivativePosNeg(xi):
+    return 2 * sigmoid_fn(xi) * (1 - sigmoid_fn(xi))
+
+
+def tanh(xi):
+    return (np.exp(xi) - np.exp(-xi)) / (np.exp(xi) + np.exp(-xi))
+
+
+def tanh_derivative(xi):
+    return (2 / (np.exp(xi) + np.exp(-xi))) ** 2
