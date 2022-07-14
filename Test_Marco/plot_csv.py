168a7c5a4d2489717b44352c72d9e5016b2b9e22
Alessandro Cecconi <59066644+aleegeco@users.noreply.github.com>
2022-07-14 17:53:44 +0200

    RISULTATI

--- Test_Marco/plot_csv.py
+++ Test_Marco/plot_csv.py
@@ -18,14 +18,21 @@ print('uu downloaded')
 
 plt.figure()
 for agent in range(NN):
-    plt.semilogy(range(epochs-1), (JJ[:-1, agent]))
+    plt.plot(range(epochs-1), (JJ[:-1, agent]))
 plt.title("Cost Function"); plt.grid()
 plt.xlabel("iteration")
 plt.ylabel("J")
 
+plt.figure()
+temp_sum_JJ = np.zeros(epochs)
+for k in range(epochs):
+    temp_sum_JJ[k] = np.sum(JJ[k])
+plt.semilogy(range(epochs-1), temp_sum_JJ[:-1])
+plt.title("Sum of the cost function"); plt.grid()
+plt.xlabel("iteration")
+plt.ylabel("$\sum_i J_i$")
 
 plt.figure()
-
 for agent in range(NN):
     temp_norm = np.zeros(epochs)
     for k in range(epochs):
@@ -33,7 +40,17 @@ for agent in range(NN):
     plt.plot(range(epochs-1), temp_norm[:-1])
 plt.title("Norm Delta Weights"); plt.grid()
 plt.xlabel("iteration")
-plt.ylabel("norm delta_u")
+plt.ylabel("$||\Delta u||$")
+
+plt.figure()
+temp_sum_norm = np.zeros(epochs)
+for k in range(epochs):
+    temp_sum_norm[k] = np.sum(delta_u[k])
+plt.semilogy(range(epochs-1), temp_sum_norm[:-1])
+plt.title("Norm Delta"); plt.grid()
+plt.xlabel("iteration")
+plt.ylabel("$\sum_i\ \Delta u_i$")
+
 
 nn = 2
 fig, axs = plt.subplots(nn, nn)
@@ -44,10 +61,10 @@ for i in range(nn):
         for agent in range(NN):
             axs[i, j].plot(range(epochs-1), uu[:-1, agent, 1, random_node, random_weight])
             axs[i, j].margins(0)
-            axs[i, j].set_title(f'Node{random_node}, Weight{random_weight}')
+            axs[i, j].set_title(f'Neuron:{random_node}, Weight:{random_weight}, layer:{1}')
 plt.setp(axs[-1, :], xlabel='iteration')
 plt.setp(axs[:, 0], ylabel='weight')
-
+fig.suptitle("Consensus over weights")
 plt.show()
 
 print('DAJE TUTTO OK')
